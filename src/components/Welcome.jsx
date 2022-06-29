a000aab3adcd74b654d9842bb8a76f93fcb19dc8
RiviCodes <jose.riveramz@udlap.mx>
2022-06-29 18:41:09 -0500

    Add background image, style elements

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Welcome() {
-  return <></>;
+  return (
+    <section
+      id="start"
+      className="flex justify-center items-center h-80 md:h-96 lg:h-screen w-auto"
+    >
+      <span className="text-white font-mono tracking-widest">BIENVENIDOS</span>
+    </section>
+  );
 }
