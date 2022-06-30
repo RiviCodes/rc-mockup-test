170e0997ebecb7583bd1b02b22c589243fd9988b
RiviCodes <jose.riveramz@udlap.mx>
2022-06-29 19:06:56 -0500

    Add button

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,9 +4,20 @@ export default function Welcome() {
   return (
     <section
       id="start"
-      className="flex justify-center items-center h-80 md:h-96 lg:h-screen w-auto"
+      className="flex flex-col  items-center h-80 md:h-96 lg:h-screen w-auto"
     >
-      <span className="text-white font-mono tracking-widest">BIENVENIDOS</span>
+      <span className="mt-12 pt-12 pb-8 text-white font-mono tracking-widest">
+        --- BIENVENIDOS ---
+      </span>
+      <h1 className="mt-12 text-4xl font-semibold text-white">
+        Lorem ipsum, dolor sit amet
+      </h1>
+      <h2 className="mb-6 text-6xl font-bold text-white">
+        consectetur adipisic
+      </h2>
+      <button className="btn rounded-md border border-white py-3 px-4 text-xs text-white">
+        CONOCE NUESTROS SERVICIOS
+      </button>
     </section>
   );
 }
