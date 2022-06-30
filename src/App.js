4617e173d7fef0d58ac489fafedcc2df8b815421
RiviCodes <jose.riveramz@udlap.mx>
2022-06-29 19:15:06 -0500

    Import and render component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Welcome from "./components/Welcome";
+import AboutUs from "./components/AboutUs";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <Welcome />
+      <AboutUs />
     </React.Fragment>
   );
 }
